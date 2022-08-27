8475263bea22762ec2e90a0708b3cb383c80575e
sebekerga <sebekerka@gmail.com>
2022-08-27 07:14:30 +0300

    added auth for posting

--- routers/nonograms.js
+++ routers/nonograms.js
@@ -1,17 +1,19 @@
 const nonogramsHelper = require('../utils/nonogramsHelper')
 const Nonogram = require('../models/nonogram')
+const logger = require('../utils/logger')
+const config = require('../utils/config')
+
+const bcrypt = require('bcrypt')
 
 const nonogramsRouter = require('express').Router()
 
-nonogramsRouter.post('/', async(request, response) => {
-    const nonogram_data = request.body    
-    const nonogram = await Nonogram.create({
-        title: nonogram_data.title,
-        description: nonogram_data.description,
-        clues: nonogramsHelper.generatePuzzleClues(nonogram_data.bitmap)
-    })
-    response.json(nonogram)
-})
+const checkCode = async code => {    
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(config.CODE, saltRounds)
+    
+    const auth = await bcrypt.compare(code, passwordHash)
+    return auth
+}
 
 nonogramsRouter.get('/summary', async(request, response) => {
     const nonograms = await Nonogram.find({})
@@ -26,8 +28,42 @@ nonogramsRouter.get('/summary', async(request, response) => {
     }))).status(202).end()
 })
 
+nonogramsRouter.post('/check', async (request, response) => {
+    response.status(auth ? 200 : 403).end()
+})
+
 nonogramsRouter.get('/:id', async(request, response) => {
     const nonogram = await Nonogram.findById(request.params.id)
+    response.json(nonogram)
+})
+
+nonogramsRouter.post('/bitmap', async(request, response) => {
+
+    const { code, nonogram_data } = request.body
+
+    const auth = await checkCode(code)
+    !auth && response.status(403).end()
+    
+    const nonogram = await Nonogram.create({
+        title: nonogram_data.title,
+        description: nonogram_data.description,
+        clues: nonogramsHelper.generatePuzzleClues(nonogram_data.bitmap)
+    })
+    response.json(nonogram)
+})
+
+nonogramsRouter.post('/clues', async(request, response) => {
+
+    const { code, nonogram_data } = request.body
+
+    const auth = await checkCode(code)
+    !auth && response.status(403).end()
+    
+    const nonogram = await Nonogram.create({
+        title: nonogram_data.title,
+        description: nonogram_data.description,
+        clues: nonogram_data.clues
+    })
     response.json(nonogram)
 })
 
--- utils/config.js
+++ utils/config.js
@@ -4,10 +4,14 @@ const logger = require('./logger')
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.NODE_ENV === 'test' ?
     process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
+const SECRET = process.env.SECRET
+const CODE = process.env.CODE
 
 logger.info('env vars: ', PORT, MONGODB_URI)
 
 module.exports = {
     MONGODB_URI,
-    PORT
+    PORT,
+    CODE,
+    SECRET
 }
